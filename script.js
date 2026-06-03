90936c80544847b59a25e4851503ee638761c6db
araschke001-commits <araschke001@students.kusd.edu>
2026-06-02 20:55:24 -0500

    Updated files to make a basic game

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const button = document.getElementById('click-button');
+const clickCount = document.getElementById('click-count');
 
+let money = 0;
+
+function buttonClick(){
+    money++;
+    
+    count.textContent = money;
+}
+
+//Main click handler
+button.addEventListener('click', function () {
+    buttonClick();
+});
